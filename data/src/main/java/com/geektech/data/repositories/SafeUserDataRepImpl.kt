8578aacb733fc818cbea07fe131b718c3542b23c
Omaraliev Eric <theslyfox43@gmail.com>
2023-04-25 17:58:12 +0600

    added SaveUserDataRepImpl

--- data/src/main/java/com/geektech/data/repositories/SafeUserDataRepImpl.kt
+++ data/src/main/java/com/geektech/data/repositories/SafeUserDataRepImpl.kt
@@ -1,4 +1,45 @@
 package com.geektech.data.repositories
 
-class SafeUserDataRepImpl {
+import android.util.Log
+import com.geektech.domain.base.constansts.Constants
+import com.geektech.domain.repositories.SaveUserDataRep
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
+import javax.inject.Inject
+
+class SafeUserDataRepImpl @Inject constructor(
+    private val firestore: FirebaseFirestore
+) : SaveUserDataRep {
+
+    override fun saveUserData(name: String) {
+        val docRef = firestore.collection("CID").document("id")
+        docRef.get(Source.SERVER).addOnSuccessListener { document ->
+            if (document != null) {
+
+                val freeId = document.data!!["count"].toString().toInt()
+                Log.d("TAG", "DocumentSnapshot data: ${document.data!!["count"]}")
+
+                val user = hashMapOf(
+                    Constants.Firebase.nameOfUsernameField to name,
+                    Constants.Firebase.userId to freeId
+                )
+                val count = hashMapOf(
+                    "count" to freeId + 1
+                )
+                firestore.collection("CID").document("id").set(count)
+                firestore.collection("Users").document(name)
+                    .set(user)
+                    .addOnSuccessListener {
+                        Log.d("TAGGER", "DocumentSnapshot successfully written!") }
+                    .addOnFailureListener { e ->
+                        Log.w("TAGGER", "Error writing document", e) }
+
+            } else {
+                Log.d("TAG", "No such document")
+            }
+
+        }.addOnFailureListener { exception ->
+            Log.d("TAG", "get failed with ", exception)
+        }
+    }
 }

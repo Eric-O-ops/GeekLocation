185b48ef6f54df2896e966a208e02c4883d53ac1
Omaraliev Eric <theslyfox43@gmail.com>
2023-04-25 17:55:54 +0600

    added SignInRepImpl

--- data/src/main/java/com/geektech/data/repositories/SignInRepImpl.kt
+++ data/src/main/java/com/geektech/data/repositories/SignInRepImpl.kt
@@ -1,4 +1,20 @@
 package com.geektech.data.repositories
 
-class SignInRepImpl {
+import com.geektech.domain.repositories.SignInRep
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import javax.inject.Inject
+
+class SignInRepImpl @Inject constructor(
+    private val auth: FirebaseAuth
+) : SignInRep {
+
+    override fun firebaseWithOneTap(idToken: String, onSuccess: () -> Unit, onError: () -> Unit) {
+        val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(firebaseCredential)
+            .addOnCompleteListener { task ->
+
+                if (task.isSuccessful) onSuccess() else onError()
+            }
+    }
 }

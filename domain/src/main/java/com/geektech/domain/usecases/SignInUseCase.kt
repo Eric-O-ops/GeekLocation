f55f1cc1c2a9818e942ac90807e773bc5788150e
Omaraliev Eric <theslyfox43@gmail.com>
2023-04-25 17:54:57 +0600

    added SignInUseCase

--- domain/src/main/java/com/geektech/domain/usecases/SignInUseCase.kt
+++ domain/src/main/java/com/geektech/domain/usecases/SignInUseCase.kt
@@ -1,4 +1,12 @@
 package com.geektech.domain.usecases
 
-class SignInUseCase {
+import com.geektech.domain.repositories.SignInRep
+import javax.inject.Inject
+
+class SignInUseCase @Inject constructor(
+    private val repository: SignInRep
+) {
+
+    operator fun invoke(idToken: String, onSuccess: () -> Unit, onError: () -> Unit) =
+        repository.firebaseWithOneTap(idToken, onSuccess, onError)
 }

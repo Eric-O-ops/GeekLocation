2923b05574a2c88c197fb6e17ad715285cd3b9b4
Omaraliev Eric <theslyfox43@gmail.com>
2023-04-25 17:56:34 +0600

    added SaveUserDataRep

---
+++ domain/src/main/java/com/geektech/domain/usecases/SaveUserDataUseCase.kt
@@ -0,0 +1,10 @@
+package com.geektech.domain.usecases
+
+import com.geektech.domain.repositories.SaveUserDataRep
+import javax.inject.Inject
+
+class SaveUserDataUseCase @Inject constructor(
+    private val repository: SaveUserDataRep
+) {
+    operator fun invoke(name: String) = repository.saveUserData(name)
+}

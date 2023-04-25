c4ab7fc1fe082368a059b4bcad03e45ff86a2adb
Omaraliev Eric <theslyfox43@gmail.com>
2023-04-25 17:48:47 +0600

    added SignInFragment

--- presentation/src/main/java/com/geektech/presentation/ui/fragments/signin/SignInFragment.kt
+++ presentation/src/main/java/com/geektech/presentation/ui/fragments/signin/SignInFragment.kt
@@ -1,7 +1,74 @@
 package com.geektech.presentation.ui.fragments.signin
 
-import androidx.fragment.app.Fragment
+import android.app.Activity
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.geektech.domain.base.constansts.Constants
+import com.geektech.presentation.R
+import com.geektech.presentation.base.BaseFragment
+import com.geektech.presentation.databinding.FragmentSignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class SignInFragment : Fragment() {
+@AndroidEntryPoint
+class SignInFragment :
+    BaseFragment<FragmentSignInBinding, SignInViewModel>(R.layout.fragment_sign_in) {
 
+    @Inject
+    lateinit var gsc: GoogleSignInClient
+    override val binding by viewBinding(FragmentSignInBinding::bind)
+    override val viewModel: SignInViewModel by viewModels()
+
+    override fun setupListener() {
+        with(binding) {
+            btnGoogle.setOnClickListener {
+                if (etInputName.text.isNotEmpty()) {
+                    etInputName.apply {
+                        setBackgroundResource(R.drawable.normal_edit_text)
+                        setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    }
+                    resultLauncher.launch(gsc.signInIntent)
+                } else {
+                    etInputName.apply {
+                        setBackgroundResource(R.drawable.error_edit_text)
+                        setCompoundDrawablesWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.ic_priority_high,
+                            0
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                val token = task.result.idToken
+                if (token != null) {
+                    viewModel.signInWithGoogle(token,
+                        onSuccess = {
+                            viewModel.saveUserData(binding.etInputName.text.toString())
+                            findNavController().navigate(R.id.action_signInFragment_to_mainFragment)
+                        },
+                        onError = {
+                            Toast.makeText(
+                                requireContext(),
+                                Constants.textError, Toast.LENGTH_SHORT
+                            ).show()
+                        })
+                } else {
+                    Log.e("TokenException", "No Token!")
+                }
+            }
+        }
 }
--- presentation/src/main/java/com/geektech/presentation/ui/fragments/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/geektech/presentation/ui/fragments/signin/SignInViewModel.kt
@@ -1,6 +1,21 @@
 package com.geektech.presentation.ui.fragments.signin
 
 import androidx.lifecycle.ViewModel
+import com.geektech.domain.usecases.SaveUserDataUseCase
+import com.geektech.domain.usecases.SignInUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SignInViewModel: ViewModel() {
+@HiltViewModel
+class SignInViewModel @Inject constructor(
+    private val signInUseCase: SignInUseCase,
+    private val saveUserDataUseCase: SaveUserDataUseCase
+
+) : ViewModel() {
+
+    fun signInWithGoogle(idToken: String, onSuccess: () -> Unit, onError: () -> Unit) =
+        signInUseCase(idToken, onSuccess, onError)
+
+    fun saveUserData(name: String) = saveUserDataUseCase(name)
+
 }

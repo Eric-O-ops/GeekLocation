e0a27712ff1b493e70d772355e0fa4789bb6339d
Omaraliev Eric <theslyfox43@gmail.com>
2023-06-05 12:40:30 +0600

    added di for preference

---
+++ app/src/main/java/com/geektech/geeklocatione/di/pref/PreferenceModule.kt
@@ -0,0 +1,27 @@
+package com.geektech.geeklocatione.di.pref
+
+import android.content.Context
+import com.geektech.data.repositories.preferences.PreferencesHelper
+import com.geektech.data.repositories.preferences.userdata.UserPreferencesData
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PreferenceModule {
+
+    @Singleton
+    @Provides
+    fun providePreferenceHelper(@ApplicationContext context: Context) =
+        PreferencesHelper(context)
+
+    @Singleton
+    @Provides
+    fun provideUserPreferenceData(preferencesHelper: PreferencesHelper) =
+        UserPreferencesData(preferencesHelper)
+
+}

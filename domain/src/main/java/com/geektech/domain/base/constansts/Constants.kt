54a7df4ebc865013226a470ee06252c40007dede
Omaraliev Eric <theslyfox43@gmail.com>
2023-06-05 12:15:36 +0600

    added Constants for firebase

--- domain/src/main/java/com/geektech/domain/base/constansts/Constants.kt
+++ domain/src/main/java/com/geektech/domain/base/constansts/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
 
     object Firebase {
         const val nameOfCollation = "Users"
+        const val nameOfLocationField = "location"
         const val nameOfUsernameField = "name"
         const val userId = "id"
     }

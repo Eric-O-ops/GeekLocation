e35eb5913915f5d84d978b05fd79ad354ecee608
Omaraliev Eric <theslyfox43@gmail.com>
2023-06-18 00:34:01 +0600

    added constant for pref storage

--- data/src/main/java/com/geektech/data/preferences/PreferencesConstants.kt
+++ data/src/main/java/com/geektech/data/preferences/PreferencesConstants.kt
@@ -1,8 +1,8 @@
-package com.geektech.data.repositories.preferences
+package com.geektech.data.preferences
 
 object PreferencesConstants {
 
     const val USER_NAME = "name"
     const val USER_ID = "id"
-
+    const val NAME_SHARE_PREF = "userData"
 }

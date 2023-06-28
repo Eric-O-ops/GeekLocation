e8d6f9a546ee6d25c0d2aa5e153be5b87ddb6ab6
Omaraliev Eric <theslyfox43@gmail.com>
2023-06-28 11:08:51 +0600

    added new constants

--- domain/src/main/java/com/geektech/domain/base/constansts/Constants.kt
+++ domain/src/main/java/com/geektech/domain/base/constansts/Constants.kt
@@ -5,19 +5,22 @@ object Constants {
     object FirebaseUsers {
 
         const val NAME_COLLECTION = "Users"
-        const val LOC_FIELD = "location"
-        const val USER_NAME_FIELD = "name"
         const val USER_ID_FIELD = "id"
+        const val USER_ACCOUNT_ID = "accountId"
+        const val USER_EMAIL = "email"
+        const val USER_NAME_FIELD = "name"
+        const val LOC_FIELD = "location"
     }
 
     object FirebaseCID {
 
         const val NAME_COLLECTION = "CID"
-        const val NAME_DOC = "id"
-        const val NAME_FIELD = "count"
+        const val NAME_DOC = "freeID"
+        const val NAME_FIELD = "id"
     }
 
     object Permissions {}
 
-    const val textError = "something was wrong"
+    const val TEXT_ERROR = "something was wrong"
+    const val SPANNABLE_TEXT_SIGN_IN = "Уже есть аккаунт?\t Войдите"
 }

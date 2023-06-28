595a846fd65b1bf54f4bc57089b03964093164a1
Omaraliev Eric <theslyfox43@gmail.com>
2023-06-28 11:22:46 +0600

    added new constant

--- data/src/main/java/com/geektech/data/preferences/PreferencesConstants.kt
+++ data/src/main/java/com/geektech/data/preferences/PreferencesConstants.kt
@@ -2,7 +2,8 @@ package com.geektech.data.preferences
 
 object PreferencesConstants {
 
-    const val USER_NAME = "name"
     const val USER_ID = "id"
+    const val USER_NAME = "name"
+    const val USER_ACCOUNT_ID = "accountId"
     const val NAME_SHARE_PREF = "userData"
 }

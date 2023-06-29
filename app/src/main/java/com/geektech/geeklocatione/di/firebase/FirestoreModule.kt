b98dd4dbe4e4f10e5c3443b6f415b6509a35c5d4
Omaraliev Eric <theslyfox43@gmail.com>
2023-06-29 13:44:33 +0600

    added singleton for firestore

--- app/src/main/java/com/geektech/geeklocatione/di/firebase/FirestoreModule.kt
+++ app/src/main/java/com/geektech/geeklocatione/di/firebase/FirestoreModule.kt
@@ -6,11 +6,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object FirestoreModule {
 
     @Provides
+    @Singleton
     fun provideFirestore() = Firebase.firestore
 }

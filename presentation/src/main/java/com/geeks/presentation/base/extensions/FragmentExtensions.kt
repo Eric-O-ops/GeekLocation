5420605f3aeec76f7a19682497fc01f3afa87b40
Omaraliev Eric <theslyfox43@gmail.com>
2023-07-11 22:36:13 +0600

    added extensions for fragment

---
+++ presentation/src/main/java/com/geeks/presentation/base/extensions/FragmentExtensions.kt
@@ -0,0 +1,30 @@
+package com.geeks.presentation.base.extensions
+
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.geeks.domain.models.LocModelDomain
+import com.geeks.presentation.R
+import com.geeks.presentation.models.LocModelUI
+import com.geeks.presentation.models.toUI
+
+fun Fragment.showAlertDialog(title: Int, massage: Int) {
+    AlertDialog.Builder(requireContext())
+        .setCancelable(false)
+        .setTitle(title)
+        .setMessage(massage)
+        .setPositiveButton(R.string.ok) { _, _ -> }
+        .create().show()
+}
+
+fun Fragment.checkSinglePermission(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        requireContext(),
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
+}
+
+fun listToListUI(list: ArrayList<LocModelDomain>): ArrayList<LocModelUI> {
+    return list.map { it.toUI() } as ArrayList<LocModelUI>
+}
